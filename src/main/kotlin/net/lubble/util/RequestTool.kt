501a813cd02f322032b34b5b58c26152c99b2c93
Emir <emirhannaneli@gmail.com>
2024-02-23 19:29:01 +0300

    added RequestTool

---
+++ src/main/kotlin/net/lubble/util/RequestTool.kt
@@ -0,0 +1,121 @@
+package net.lubble.util
+
+import jakarta.servlet.http.HttpServletRequest
+import org.slf4j.LoggerFactory
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass
+import org.springframework.stereotype.Component
+
+@Component
+@ConditionalOnClass(HttpServletRequest::class)
+open class RequestTool(private val request: HttpServletRequest) {
+    private val logger = LoggerFactory.getLogger(RequestTool::class.java)
+    init {
+        logger.info("RequestTool initialized.")
+    }
+    fun hasHeader(header: String): Boolean {
+        return request.headerNames.toList().contains(header)
+    }
+
+    fun header(header: String): String? {
+        return request.getHeader(header)
+    }
+
+    fun headers(): List<String> {
+        return request.headerNames.toList()
+    }
+
+    fun method(): String {
+        return request.method
+    }
+
+    fun path(): String {
+        return request.requestURI
+    }
+
+    fun query(): String? {
+        return request.queryString
+    }
+
+    fun url(): String {
+        return request.requestURL.toString()
+    }
+
+    fun ip(): String {
+        return request.remoteAddr
+    }
+
+    fun port(): Int {
+        return request.remotePort
+    }
+
+    fun protocol(): String {
+        return request.protocol
+    }
+
+    fun scheme(): String {
+        return request.scheme
+    }
+
+    fun host(): String {
+        return request.remoteHost
+    }
+
+    fun userAgent(): String {
+        return request.getHeader("User-Agent")
+    }
+
+    fun referer(): String {
+        return request.getHeader("Referer")
+    }
+
+    fun accept(): String {
+        return request.getHeader("Accept")
+    }
+
+    fun acceptLanguage(): String {
+        return request.getHeader("Accept-Language")
+    }
+
+    fun acceptEncoding(): String {
+        return request.getHeader("Accept-Encoding")
+    }
+
+    fun acceptCharset(): String {
+        return request.getHeader("Accept-Charset")
+    }
+
+    fun cookie(name: String): String? {
+        val cookies = request.cookies
+        if (cookies != null) {
+            for (cookie in cookies) {
+                if (cookie.name == name) {
+                    return cookie.value
+                }
+            }
+        }
+        return null
+    }
+
+    fun cookies(): List<String> {
+        val cookies = request.cookies
+        val list = mutableListOf<String>()
+        if (cookies != null) {
+            for (cookie in cookies) {
+                list.add(cookie.name)
+            }
+        }
+        return list
+    }
+
+    fun param(name: String): String? {
+        return request.getParameter(name)
+    }
+
+    fun params(): List<String> {
+        return request.parameterMap.keys.toList()
+    }
+
+    fun body(): String {
+        return request.reader.readText()
+    }
+}
