cb7f925c74cb186fbf207c86cc428f893efde088
Emir <emirhannaneli@gmail.com>
2024-03-28 10:32:29 +0300

    update

--- src/main/kotlin/net/lubble/util/ReFormat.kt
+++ src/main/kotlin/net/lubble/util/ReFormat.kt
@@ -3,56 +3,56 @@ package net.lubble.util
 import org.springframework.context.i18n.LocaleContextHolder
 import java.util.*
 
-class ReFormat(private var value: String?) {
+class ReFormat(private var value: String) {
     private val locale = LocaleContextHolder.getLocale() ?: Locale.ENGLISH
     fun upper(): ReFormat {
-        value = value?.uppercase(locale)
+        value = value.uppercase(locale)
         return this
     }
 
     fun upper(locale: Locale): ReFormat {
-        value = value?.uppercase(locale)
+        value = value.uppercase(locale)
         return this
     }
 
 
     fun lower(): ReFormat {
-        value = value?.lowercase(locale)
+        value = value.lowercase(locale)
         return this
     }
 
     fun lower(locale: Locale): ReFormat {
-        value = value?.lowercase(locale)
+        value = value.lowercase(locale)
         return this
     }
 
 
     fun trim(): ReFormat {
-        value = value?.trim()
+        value = value.trim()
         return this
     }
 
     fun capitalize(): ReFormat {
-        value = value?.replaceFirstChar { it.titlecase(locale) }
+        value = value.replaceFirstChar { it.titlecase(locale) }
         return this
     }
 
     fun capitalize(locale: Locale): ReFormat {
-        value = value?.replaceFirstChar { it.titlecase(locale) }
+        value = value.replaceFirstChar { it.titlecase(locale) }
         return this
     }
 
     fun decapitate(): ReFormat {
-        value = value?.replaceFirstChar { it.lowercase(locale) }
+        value = value.replaceFirstChar { it.lowercase(locale) }
         return this
     }
 
     fun decapitate(locale: Locale): ReFormat {
-        value = value?.replaceFirstChar { it.lowercase(locale) }
+        value = value.replaceFirstChar { it.lowercase(locale) }
         return this
     }
 
-    fun format(): String? {
+    fun format(): String {
         return value
     }
 }
