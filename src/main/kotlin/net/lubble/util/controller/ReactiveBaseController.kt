984e2240dc20a58a50946e199fc1b6459e9480b7
Emirhan Naneli <emirhannaneli@gmail.com>
2024-05-01 00:06:22 +0300

    added ReactiveBaseController

---
+++ src/main/kotlin/net/lubble/util/controller/ReactiveBaseController.kt
@@ -0,0 +1,151 @@
+package net.lubble.util.controller
+
+import jakarta.validation.Valid
+import net.lubble.util.PageResponse
+import net.lubble.util.Response
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
+import reactor.core.publisher.Mono
+
+/**
+ * This interface defines the base operations for a reactive controller.
+ * It includes CRUD operations, as well as additional operations for archiving, unarchiving, and managing a recycle bin.
+ *
+ * @param C The type of the object used in the create operation.
+ * @param U The type of the object used in the update operation.
+ * @param R The type of the object returned in the response.
+ * @param P The type of the object used as parameters in the find operations.
+ */
+interface ReactiveBaseController<C, U, R, P> {
+    /**
+     * Create a new entity.
+     *
+     * @param create The object containing the data to create the new entity.
+     * @return A Mono that emits the ResponseEntity of the created entity.
+     */
+    @PostMapping
+    fun create(@RequestBody @Valid create: C): Mono<ResponseEntity<R>>
+
+    /**
+     * Find an entity by its ID.
+     *
+     * @param id The ID of the entity to find.
+     * @return A Mono that emits the ResponseEntity of the found entity.
+     */
+    @GetMapping("{id}")
+    fun findById(@PathVariable id: String): Mono<ResponseEntity<R>>
+
+    /**
+     * Find all entities.
+     *
+     * @param params The parameters to use in the find operation.
+     * @return A Mono that emits the ResponseEntity of the PageResponse containing all found entities.
+     */
+    @GetMapping
+    fun findAll(@Valid params: P): Mono<ResponseEntity<PageResponse>>
+
+    /**
+     * Update an existing entity.
+     *
+     * @param id The ID of the entity to update.
+     * @param update The object containing the data to update the entity.
+     * @return A Mono that emits the ResponseEntity of the updated entity.
+     */
+    @PutMapping("{id}")
+    fun update(@PathVariable id: String, @RequestBody @Valid update: U): Mono<ResponseEntity<R>>
+
+    /**
+     * Delete an entity.
+     *
+     * @param id The ID of the entity to delete.
+     * @return A Mono that emits the ResponseEntity of the Response indicating the result of the operation.
+     */
+    @DeleteMapping("{id}")
+    fun delete(@PathVariable id: String): Mono<ResponseEntity<Response>>
+
+    // Additional operations for archiving, unarchiving, and managing a recycle bin are defined below.
+    // These operations throw UnsupportedOperationException by default, and should be overridden in the implementing class if needed.
+
+    /**
+     * Find all archived entities.
+     *
+     * @param params The parameters to use in the find operation.
+     * @return A Mono that emits the ResponseEntity of the PageResponse containing all found entities.
+     * @throws UnsupportedOperationException If the method is not overridden in the implementing class.
+     */
+    @GetMapping("archive")
+    fun findAllArchived(@Valid params: P): Mono<ResponseEntity<PageResponse>> {
+        throw UnsupportedOperationException()
+    }
+
+    /**
+     * Archive an entity.
+     *
+     * @param id The ID of the entity to archive.
+     * @return A Mono that emits the ResponseEntity of the Response indicating the result of the operation.
+     * @throws UnsupportedOperationException If the method is not overridden in the implementing class.
+     */
+    @PutMapping("{id}/archive")
+    fun archive(@PathVariable id: String): Mono<ResponseEntity<Response>> {
+        throw UnsupportedOperationException()
+    }
+
+    /**
+     * Unarchive an entity.
+     *
+     * @param id The ID of the entity to unarchive.
+     * @return A Mono that emits the ResponseEntity of the Response indicating the result of the operation.
+     * @throws UnsupportedOperationException If the method is not overridden in the implementing class.
+     */
+    @PutMapping("{id}/unarchive")
+    fun unarchive(@PathVariable id: String): Mono<ResponseEntity<Response>> {
+        throw UnsupportedOperationException()
+    }
+
+    /**
+     * Get the recycle bin.
+     *
+     * @param params The parameters to use in the find operation.
+     * @return A Mono that emits the ResponseEntity of the PageResponse containing all found entities.
+     * @throws UnsupportedOperationException If the method is not overridden in the implementing class.
+     */
+    @GetMapping("recycle-bin")
+    fun recycleBin(@Valid params: P): Mono<ResponseEntity<PageResponse>> {
+        throw UnsupportedOperationException()
+    }
+
+    /**
+     * Clear the recycle bin.
+     *
+     * @return A Mono that emits the ResponseEntity of the Response indicating the result of the operation.
+     * @throws UnsupportedOperationException If the method is not overridden in the implementing class.
+     */
+    @DeleteMapping("recycle-bin/clear")
+    fun clearRecycleBin(): Mono<ResponseEntity<Response>> {
+        throw UnsupportedOperationException()
+    }
+
+    /**
+     * Restore an entity from the recycle bin.
+     *
+     * @param id The ID of the entity to restore.
+     * @return A Mono that emits the ResponseEntity of the Response indicating the result of the operation.
+     * @throws UnsupportedOperationException If the method is not overridden in the implementing class.
+     */
+    @PutMapping("{id}/restore")
+    fun restore(@PathVariable id: String): Mono<ResponseEntity<Response>> {
+        throw UnsupportedOperationException()
+    }
+
+    /**
+     * Permanently delete an entity.
+     *
+     * @param id The ID of the entity to delete.
+     * @return A Mono that emits the ResponseEntity of the Response indicating the result of the operation.
+     * @throws UnsupportedOperationException If the method is not overridden in the implementing class.
+     */
+    @DeleteMapping("{id}/permanently")
+    fun deletePermanently(@PathVariable id: String): Mono<ResponseEntity<Response>> {
+        throw UnsupportedOperationException()
+    }
+}
