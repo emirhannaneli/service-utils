4b65626ac5487ffd007d0dd28c3eaeb2c847a789
Emirhan Naneli <emirhannaneli@gmail.com>
2024-05-18 00:52:01 +0300

    update

--- src/main/kotlin/net/lubble/util/model/ParameterModel.kt
+++ src/main/kotlin/net/lubble/util/model/ParameterModel.kt
@@ -1,9 +1,15 @@
 package net.lubble.util.model
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.cfg.MapperConfig
+import com.fasterxml.jackson.databind.introspect.Annotated
+import com.fasterxml.jackson.databind.introspect.JacksonAnnotationIntrospector
 import jakarta.persistence.Transient
+import net.lubble.util.AppContextUtil
 import net.lubble.util.helper.EnumHelper
 import net.lubble.util.spec.PaginationSpec
+import java.util.*
 
 /**
  * This class represents a model for parameters.
@@ -13,7 +19,7 @@ import net.lubble.util.spec.PaginationSpec
  * @property sortBy The field to sort by. It is transient, meaning it is not persisted in the database.
  * @property sortOrder The order to sort in. It is transient, meaning it is not persisted in the database.
  */
-@JsonIgnoreProperties(value = ["page", "size", "sort", "order" ,"search"])
+@JsonIgnoreProperties(value = ["page", "size", "sortBy", "sortOrder", "search"])
 open class ParameterModel : PaginationSpec() {
     @Transient
     var search: String? = null
@@ -25,6 +31,18 @@ open class ParameterModel : PaginationSpec() {
 
     @Transient
     var sortOrder: SortOrder = SortOrder.ASC
+
+    fun toKey(): String {
+        val mapper = AppContextUtil.bean(ObjectMapper::class.java).apply {
+            setAnnotationIntrospector(object : JacksonAnnotationIntrospector() {
+                override fun findPropertyIgnoralByName(config: MapperConfig<*>?, a: Annotated?): JsonIgnoreProperties.Value {
+                    return JsonIgnoreProperties.Value.empty()
+                }
+            })
+        }
+        val json = mapper.writeValueAsString(this)
+        return Base64.getEncoder().encodeToString(json.toByteArray())
+    }
 }
 
 /**
--- src/main/kotlin/net/lubble/util/spec/PaginationSpec.kt
+++ src/main/kotlin/net/lubble/util/spec/PaginationSpec.kt
@@ -13,19 +13,19 @@ import org.springframework.data.domain.Sort
  */
 open class PaginationSpec {
     @Transient
-    var page: Int? = 1
-        get() = field?.coerceAtLeast(1)
+    var page: Int = 1
+        get() = field.coerceAtLeast(1)
     @Transient
-    var size: Int? = 10
-        get() = field?.coerceAtLeast(1)
+    var size: Int = 10
+        get() = field.coerceAtLeast(1)
 
     /**
      * Converts this PaginationSpec into a Pageable.
      * @return The Pageable representing this PaginationSpec.
      */
     fun ofPageable(): Pageable {
-        val page = (this.page?.minus(1) ?: 0).coerceAtLeast(0)
-        val size = this.size?.coerceAtLeast(1) ?: 1
+        val page = this.page.minus(1).coerceAtLeast(0)
+        val size = this.size.coerceAtLeast(1)
         return PageRequest.of(page, size)
     }
 
@@ -34,8 +34,8 @@ open class PaginationSpec {
      * @param sort The sort order to apply.
      */
     fun ofPageable(sort: Sort): Pageable {
-        val page = (this.page?.minus(1) ?: 0).coerceAtLeast(0)
-        val size = this.size?.coerceAtLeast(1) ?: 1
+        val page = this.page.minus(1).coerceAtLeast(0)
+        val size = this.size.coerceAtLeast(1)
         return PageRequest.of(page, size, sort)
     }
 }
