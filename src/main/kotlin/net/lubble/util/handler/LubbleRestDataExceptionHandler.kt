5f5bfc5834136e1b59a2bbb89cfccbbac877299b
Emirhan Naneli <emirhannaneli@gmail.com>
2024-10-16 16:42:44 +0300

    update

--- src/main/kotlin/net/lubble/util/handler/LubbleRestDataExceptionHandler.kt
+++ src/main/kotlin/net/lubble/util/handler/LubbleRestDataExceptionHandler.kt
@@ -1,6 +1,7 @@
 package net.lubble.util.handler
 
 import jakarta.annotation.Priority
+import jakarta.validation.ConstraintViolationException
 import net.lubble.util.Response
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
@@ -9,6 +10,8 @@ import org.springframework.http.HttpStatus.BAD_REQUEST
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.bind.annotation.ExceptionHandler
+import kotlin.collections.last
+import kotlin.text.trim
 
 @Priority(0)
 @ControllerAdvice
@@ -27,6 +30,19 @@ class LubbleRestDataExceptionHandler {
             "0x000400-6",
             mapOf(
                 "message" to e.message
+            )
+        ).build()
+    }
+
+    @ExceptionHandler(ConstraintViolationException::class)
+    fun handleConstraintViolationException(e: ConstraintViolationException): ResponseEntity<Response> {
+        val message = e.message?.split(":")?.last()?.trim() ?: "global.exception.invalid.param"
+        return Response(
+            message,
+            BAD_REQUEST,
+            "0x000400-6",
+            mapOf(
+                "fields" to e.constraintViolations.map { it.propertyPath.toString() }
             )
         ).build()
     }
