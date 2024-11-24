a3d60b636f42d886288659634bf72176c773c388
Emir <emirhannaneli@gmail.com>
2024-11-24 11:18:37 +0300

    update

--- src/main/kotlin/net/lubble/util/annotation/security/SecuredParamProcessor.kt
+++ src/main/kotlin/net/lubble/util/annotation/security/SecuredParamProcessor.kt
@@ -1,22 +1,29 @@
 package net.lubble.util.annotation.security
 
 import net.lubble.util.exception.AccessDenied
-import net.lubble.util.model.BaseJPAModel
-import net.lubble.util.model.BaseMongoModel
 import org.aspectj.lang.JoinPoint
-import org.aspectj.lang.annotation.After
+import org.aspectj.lang.annotation.AfterReturning
 import org.aspectj.lang.annotation.Aspect
 import org.aspectj.lang.annotation.Before
 import org.springframework.expression.spel.standard.SpelExpressionParser
 import org.springframework.expression.spel.support.StandardEvaluationContext
+import org.springframework.http.ResponseEntity
 import org.springframework.security.authentication.AnonymousAuthenticationToken
+import org.springframework.security.core.Authentication
 import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.stereotype.Component
+import java.lang.reflect.Field
 
 @Aspect
 @Component
 class SecuredParamProcessor() {
 
+    /**
+     * Pre-authorize parameters before the execution of the target method.
+     *
+     * @param point the join point representing the method execution
+     * @throws AccessDenied if the user does not have the required permissions
+     */
     @Before("target(net.lubble.util.controller.BaseController)")
     fun preAuthorizeParam(point: JoinPoint) {
         val auth = SecurityContextHolder.getContext().authentication
@@ -26,31 +33,32 @@ class SecuredParamProcessor() {
         context.setVariable("auth", auth)
         context.setVariable("authentication", auth)
 
-        val args = point.args.filter { arg ->
-            arg is BaseJPAModel.SearchParams || arg is BaseMongoModel.SearchParams
-        }
-
-        args.forEach { arg ->
+        point.args.forEach { arg ->
             arg::class.java.declaredFields.forEach { field ->
+                val securedParam = field.getAnnotation(PreAuthorizeParam::class.java) ?: return@forEach
+
                 field.isAccessible = true
 
                 val value = field.get(arg) ?: return@forEach
                 context.setVariable(field.name, value)
 
-                val securedParam = field.getAnnotation(PreAuthorizeParam::class.java)
-                val expression = securedParam?.value ?: return@forEach
-
                 if (auth !is AnonymousAuthenticationToken && auth.isAuthenticated)
-                    parser.parseExpression(expression).getValue(context, Boolean::class.java)?.let { hasPermission ->
-                        if (!hasPermission) throw AccessDenied()
-                    }
+                    parser.parseExpression(securedParam.value).getValue(context, Boolean::class.java)
+                        ?.let { hasPermission ->
+                            if (!hasPermission) throw AccessDenied()
+                        }
                 else throw AccessDenied()
             }
         }
     }
 
-    @After("target(net.lubble.util.controller.BaseController)")
-    fun postAuthorizeParam(point: JoinPoint) {
+    /**
+     * Post-authorize parameters after the execution of the target method.
+     *
+     * @param result the result of the method execution
+     */
+    @AfterReturning(pointcut = "target(net.lubble.util.controller.BaseController)", returning = "result")
+    fun postAuthorizeParam(result: Any) {
         val auth = SecurityContextHolder.getContext().authentication
         val context = StandardEvaluationContext(auth)
         val parser = SpelExpressionParser()
@@ -58,26 +66,65 @@ class SecuredParamProcessor() {
         context.setVariable("auth", auth)
         context.setVariable("authentication", auth)
 
-        val args = point.args.filter { arg ->
-            arg is BaseJPAModel.SearchParams || arg is BaseMongoModel.SearchParams
-        }
-
-        args.forEach { arg ->
-            arg::class.java.declaredFields.forEach { field ->
-                field.isAccessible = true
-
-                val value = field.get(arg) ?: return@forEach
-                context.setVariable(field.name, value)
-
-                val securedParam = field.getAnnotation(PostAuthorizeParam::class.java)
-                val expression = securedParam?.value ?: return@forEach
-
-                if (auth !is AnonymousAuthenticationToken && auth.isAuthenticated)
-                    parser.parseExpression(expression).getValue(context, Boolean::class.java)?.let { hasPermission ->
-                        if (!hasPermission) throw AccessDenied()
-                    }
-                else throw AccessDenied()
+        if (result is ResponseEntity<*>) {
+            result.body?.let { body ->
+                sanitizeFields(body, context, parser)
             }
+        } else {
+            sanitizeFields(result, context, parser)
+        }
+    }
+
+    /**
+     * Sanitize fields of the given object based on security annotations.
+     *
+     * @param obj the object to sanitize
+     * @param context the evaluation context
+     * @param parser the Spring-EL expression parser
+     */
+    private fun sanitizeFields(obj: Any, context: StandardEvaluationContext, parser: SpelExpressionParser) {
+        val fields = obj::class.java.declaredFields
+
+        fields.forEach { field ->
+            field.isAccessible = true
+
+            val value = field.get(obj) ?: return@forEach
+
+            if (!field.type.isPrimitive && !field.type.isEnum && field.type != String::class.java)
+                sanitizeFields(value, context, parser)
+
+            val securedParam = field.getAnnotation(PostAuthorizeParam::class.java) ?: return@forEach
+            val expression = securedParam.value
+
+            val auth = context.lookupVariable("auth") as Authentication
+
+            if (auth !is AnonymousAuthenticationToken && auth.isAuthenticated) {
+                val hasPermission = parser.parseExpression(expression).getValue(context, Boolean::class.java) == true
+
+                if (!hasPermission) censorField(field, obj)
+            } else censorField(field, obj)
+        }
+    }
+
+    /**
+     * Censor the value of the given field in the object.
+     *
+     * @param field the field to censor
+     * @param obj the object containing the field
+     */
+    private fun censorField(field: Field, obj: Any) {
+        field.isAccessible = true
+        val value = field.get(obj) ?: return
+        when (field.type) {
+            String::class.java -> {
+                val str = value as String
+                var censored = str.replace(Regex("[^a-zA-Z0-9_\\.]"), "*")
+                if (str.length > 3) censored = str.substring(0, 3) + censored.substring(3)
+                field.set(obj, censored)
+            }
+            List::class.java -> field.set(obj, emptyList<Any>())
+            Map::class.java -> field.set(obj, emptyMap<Any, Any>())
+            else -> field.set(obj, null)
         }
     }
 }
